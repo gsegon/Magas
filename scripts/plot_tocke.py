d98b8241ab89bfe7d007f8fcb828e2f62280e3ad
gsegon <gordan.segon@gmail.com>
2023-06-16 12:03:21 +0200

    * Added dev files for OverlapPointsTransformation

---
+++ scripts/plot_tocke.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+if __name__ == '__main__':
+    tocke1 = pd.read_csv('/home/gordan/Programs/solver/scripts/tocke1.csv')
+    tocke2 = pd.read_csv('/home/gordan/Programs/solver/scripts/tocke2.csv')
+
+    colors = ['r', 'g', 'b', 'm']
+
+    plt.figure()
+    for index, (x, y) in enumerate(zip(tocke1['x'], tocke1['y'])):
+        plt.plot(x, y, 'o', color=colors[index])
+
+    for index, (x, y) in enumerate(zip(tocke2['x'], tocke2['y'])):
+        plt.plot(x, y, 'o', color=colors[index], alpha=0.5)
+
+    plt.gca().set_aspect('equal')
+    plt.show()
