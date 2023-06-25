62b96f4c0ffa067c5ac9ee258b4e2bb125d99601
gsegon <gordan.segon@gmail.com>
2023-06-25 15:21:57 +0200

    * ArkkioScalarPostprocessor added

---
+++ src/postprocessing/src/processors/ArkkioScalarPostprocessor.cpp
@@ -0,0 +1,91 @@
+//
+// Created by gordan on 5/16/23.
+//
+#include <variant>
+
+#include <deal.II/lac/vector.h>
+#include <deal.II/grid/tria.h>
+#include <deal.II/dofs/dof_handler.h>
+#include <deal.II/fe/fe_q.h>
+#include <deal.II/fe/fe_values.h>
+#include <deal.II/base/quadrature_lib.h>
+#include "exprtk.hpp"
+
+#include "processors/ArkkioScalarPostprocessor.h"
+
+typedef exprtk::symbol_table<double> symbol_table_t;
+typedef exprtk::expression<double>   expression_t;
+typedef exprtk::parser<double>       parser_t;
+
+using namespace dealii;
+
+template class ArkkioScalarPostprocessor<2>;
+
+
+
+template <int dim>
+ArkkioScalarPostprocessor<dim>::ArkkioScalarPostprocessor(const unsigned int mat_id, const std::unordered_map<int, double>& nu_map) {
+    this->nu_map_ptr = &nu_map;
+    this->mat_id = mat_id;
+}
+
+
+template<int dim>
+void ArkkioScalarPostprocessor<dim>::process(const Triangulation<dim>&  triangulation,
+                                                 const Vector<double>&      solution,
+                                                 const FE_Q<dim>&           fe,
+                                                 double& result) {
+
+    triangulation_ptr = &triangulation;
+    solution_ptr = &solution;
+    fe_ptr = &fe;
+
+    std::vector<Point<dim>> q_points;
+    DoFHandler<dim> dof_handler(*triangulation_ptr);
+    dof_handler.distribute_dofs(*fe_ptr);
+    QGauss<dim> quadrature_formula(fe_ptr->degree +1);
+    FEValues<dim> fe_values(*fe_ptr, quadrature_formula, update_values | update_gradients | update_quadrature_points |
+                                                         update_JxW_values);
+    std::vector<Tensor<1, dim>> solution_gradients(quadrature_formula.size());
+    std::vector<double> solution_at_cell(quadrature_formula.size());
+
+    // Checks
+    double nu;
+    double nu_0 = 795774.715025;
+    if (nu_map_ptr)
+        nu = (*nu_map_ptr).at(mat_id);
+
+    AssertThrow(std::abs(nu-nu_0) < 1e-3, ExcInternalError())
+
+    //
+    double r1=std::numeric_limits<double>::infinity();
+    double r2=-std::numeric_limits<double>::infinity();
+    result = 0;
+    for (auto& cell : dof_handler.active_cell_iterators()){
+        if (cell->material_id() == mat_id){
+            fe_values.reinit(cell);
+            fe_values.get_function_gradients(*solution_ptr, solution_gradients);
+            fe_values.get_function_values(*solution_ptr, solution_at_cell);
+            q_points = fe_values.get_quadrature_points();
+
+            for (auto vertex_index : GeometryInfo<dim>::vertex_indices()){
+                double r = cell->vertex(vertex_index).norm();
+                if(r < r1) r1 = r;
+                if(r > r2) r2 = r;
+            }
+            for (auto q: fe_values.quadrature_point_indices()){
+                auto x = q_points[q][0];
+                auto y = q_points[q][1];
+                auto Bx = solution_gradients[q][1];
+                auto By = -solution_gradients[q][0];
+                auto JxW = fe_values.JxW(q);
+                result += (Bx*By*x*x +(By*By-Bx*Bx)*x*y-Bx*By*y*y)/sqrt(x*x+y*y)*JxW;
+            }
+        }
+    }
+
+    result *= nu/(r2-r1);
+
+}
+
+
---
+++ src/postprocessing/test/test_processors/test_ArkkioScalarPostprocessor.cpp
@@ -0,0 +1,55 @@
+//
+// Created by gordan on 3/12/23.
+//
+#include <gtest/gtest.h>
+#include <tuple>
+#include <unordered_map>
+#include <string>
+#include <any>
+#include <fstream>
+
+#include "LinearSolver.h"
+#include "processors/ArkkioScalarPostprocessor.h"
+
+TEST(ArkkioScalarPostprocessor, torque_benchmark_kelvin_1){
+
+    std::string test_mesh = "/home/gordan/Programs/solver/examples/torque_benchmark_kelvin_1/torque_benchmark_kelvin_1.msh";
+    std::unordered_map<int, double> nu_map{{3, 795774.715025},
+                                           {4, 795774.715025},
+                                           {5, 795774.715025},
+                                           {6, 795774.715025},
+                                           {8, 795774.715025}};
+
+    std::unordered_map<int, std::variant<double, std::pair<double, double>>> f_map{{3, std::pair<double, double>{0.0, 1000000.0}},
+                                                                                   {4, 0},
+                                                                                   {5, 0},
+                                                                                   {6, 0},
+                                                                                   {8, std::pair<double, double>{-1591549.43091895, 0.0}}
+                                                                                   };
+    std::unordered_map<int, double> dc_map{{7, 0}};
+
+    std::unordered_map<std::string, std::vector<unsigned int>> per_map{{"periodic-circle", {1, 2}}};
+
+    LinearSolver<2> solver;
+    solver.read_mesh(test_mesh);
+    solver.set_nu_map(nu_map);
+    solver.set_f_map(f_map);
+    solver.set_dc_map(dc_map);
+    solver.set_per_map(per_map);
+    solver.setup_system();
+    solver.assemble_system();
+    solver.solve();
+
+    ArkkioScalarPostprocessor<2> arkkio{5, nu_map};
+
+    double result;
+    arkkio.process(solver.get_triangulation(), solver.get_solution(), solver.get_fe(), result);
+
+    std::cout << "result: " << result << std::endl;
+    std::cout << "result*2e-2: " << result*2e-2 << std::endl;
+
+    ASSERT_NEAR(result*2e-2, 1.0, 1e-3);
+
+}
+
+
