1b9799bd805bf4b322c0e8cca2c848a2382ebc20
gsegon <gordan.segon@gmail.com>
2023-07-03 16:38:09 +0200

    * ScalarPostProcessorFactory update

---
+++ src/postprocessing/include/processors/RegexPatterns.h
@@ -0,0 +1,21 @@
+//
+// Created by gordan on 7/3/23.
+//
+
+#ifndef MAGAS_REGEXPATTERNS_H
+#define MAGAS_REGEXPATTERNS_H
+
+#include <string>
+#include <vector>
+
+namespace RegexPatterns{
+
+    std::string arkkio{R"((Arkkio)\((.*),\s*\((.*),(.*)\)\))"};
+    std::string b_abs{R"((Babs)\((.*),\s*(.*)\))"};
+    std::string lorentz{R"((LorentzForce)\((.*),\s*(.*)\))"};
+    std::string flux_linkage{R"((FluxLinkage)\((.*)\))"};
+
+    std::vector<std::string> patterns{arkkio, b_abs, lorentz, flux_linkage};
+}
+
+#endif //MAGAS_REGEXPATTERNS_H
