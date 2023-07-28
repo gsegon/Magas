f37d036a0bc26de2c79abc932bad58a9a3c5099d
gsegon <gordan.segon@gmail.com>
2023-07-28 14:16:41 +0200

    * Implemented the requested add periodicity constraint. (example)

--- src/material/test/test_LinearBHCurve.cpp
+++ src/material/test/test_LinearBHCurve.cpp
@@ -12,3 +12,12 @@ TEST(LinearBHCurve, basic) {
     ASSERT_EQ(bh1.get_nu(0.5), 795774.715025);
     ASSERT_EQ(bh1.get_nu_prime(0.5), 0.0);
 }
+
+TEST(LinearBHCurve, basic2) {
+
+    LinearNuCurve bh1{100};
+
+    ASSERT_EQ(bh1.get_nu(0.5), 100);
+    ASSERT_EQ(bh1.get_nu_prime(0.5), 0.0);
+}
+
